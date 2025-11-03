d5e3045f76814f77b0f14cfb2c8b25e854825683
tingyup1 <142997103+tingyup1@users.noreply.github.com>
2025-11-03 11:37:01 +0200

    add attack effect when player attack
    -play on awake attack effect when player attack.
    -modify rotation of attackEffect and sorting layers to fit the player movement.

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -7,15 +7,18 @@ namespace Game.Player
     public class PlayerAttack : MonoBehaviour
     {
         [SerializeField] private GameObject attackHitbox;
+        [SerializeField] private GameObject attackEffect;
         private float hitboxWindow = 0.10f;
         private float hardstun = 0.15f;
         private float attackLockDuration = 0.41f;
         private float recovery = 0.16f;
         private float inputBufferWindow = 0.12f;
         private Vector2 hitboxOffset = new Vector2(1f, 0.5f);
+        private Vector3 effectOffset = new Vector3(1.5f, 0.8f, 0f);
 
 
         private GameObject activeHitbox;
+        private GameObject activeEffect;
         private Transform playerTransform;
 
         public delegate void AttackEvent();
@@ -69,6 +72,31 @@ namespace Game.Player
                 queuedAttack = false;
                 attackStartTime = Time.time;
                 OnAttackPerformed?.Invoke();
+
+                //spawn attact effect
+                if(attackEffect != null)
+                {
+                    activeEffect = Instantiate(attackEffect, playerTransform);
+                    activeEffect.transform.localPosition = effectOffset;
+
+                    //effect flip
+                    Vector3 effectEuler = activeEffect.transform.localEulerAngles;
+                    effectEuler.y = (playerTransform.localScale.x < 0f) ? 180f : 0f;
+                    activeEffect.transform.localEulerAngles = effectEuler;
+
+                    //play on awake effect
+                    ParticleSystem ps = activeEffect.GetComponent<ParticleSystem>();
+                    if (ps != null && !ps.isPlaying)
+                    {
+                        ps.Play();
+                        Destroy(activeEffect, ps.main.duration + ps.main.startLifetime.constantMax);
+                    }
+                    else
+                    {
+                       
+                        Destroy(activeEffect, 1.1f);
+                    }
+                }
 
                 //spawn hitbox
                 if (attackHitbox != null)
